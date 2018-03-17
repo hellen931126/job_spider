b8764e5525fa489e43fd056d32ccacd119fd25cd
hellen <15652531860@163.com>
2018-03-17 17:08:28 +0800

    add multi-pages spider & update README.md

--- job_spider.py
+++ job_spider.py
@@ -10,49 +10,50 @@ from bs4 import BeautifulSoup
 headers = {'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'} 
 
 class Job_spider(): 
-    url = "http://www.lagou.com/zhaopin/"
+    base_url = "http://www.lagou.com/zhaopin/"
     
     def __init__(self, keyword):
         self.keyword = keyword
 
-    def __fetch_content(self):
-        htmls = requests.get(Job_spider.url + self.keyword + "/1/", headers = headers, proxies=random.choice(proxys))
-        htmls = htmls.text
-        return htmls
+    def __get_urls(self, end):
+        urls = []
+        for page in range(end):
+            urls.append(Job_spider.base_url + self.keyword + "/" + str(page+1) + "/")
+        return urls
 
-    def __analysis(self, htmls):
+    def __analysis(self, url):
+        htmls = requests.get(url, headers = headers,proxies=random.choice(proxys)).text
         soup = BeautifulSoup(htmls, "html5lib")
         contents = soup.select(".item_con_list li")
-        
+
         jobs = []
         for content in contents:
-            jobs.append({
-                "title": content.select("h3")[0].text, 
-                "salary": content.select(".money")[0].text,
-                "company": {
+            title = content.select("h3")[0].text
+            salary = content.select(".money")[0].text
+            company = {
                     "company_name": content.select(".company_name a")[0].text,
                     "industry": content.select(".industry")[0].text.strip()
-                },
-                "location": content.select(".add em")[0].text, 
-                "tags": [tag.text for tag in content.select(".list_item_bot .li_b_l span")],
-                "welfare": content.select(".li_b_r")[0].text,
-                "time": content.select(".format-time")[0].text     
-            })
+                }
+            location = content.select(".add em")[0].text
+            tags = [tag.text for tag in content.select(".list_item_bot .li_b_l span")]
+            welfare = content.select(".li_b_r")[0].text
+            time = content.select(".format-time")[0].text
+            info=[title, salary, company, location, tags, welfare, time]
+            jobs.append(info)
         return jobs
     
     def __save(self, jobs):
-        f = open("data.csv", "w")
-        writer = csv.DictWriter(f, jobs[0].keys())
-        writer.writeheader()
-        for job in jobs:
-            writer.writerow(job)
+        f = open("data.csv", "a")
+        writer = csv.writer(f)
+        writer.writerows(jobs)
         f.close()
 
     def go(self):
-        htmls = self.__fetch_content()
-        jobs = self.__analysis(htmls)
-        self.__save(jobs)
-
+        urls = self.__get_urls(end=30)
+        for url in urls:
+            jobs = self.__analysis(url)
+            self.__save(jobs)
+        
 
 spider = Job_spider("Python")
 spider.go()
